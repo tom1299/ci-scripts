b21991dbe9b340d5a4d00eff3eb029601c6739d1
tom1299 <tom1299@gmx.de>
2021-10-05 11:48:33 +0200

    Report generation now working

--- xray-scanning/scan.py
+++ xray-scanning/scan.py
@@ -1,9 +1,24 @@
 import argparse
+from datetime import datetime
+import json
 import requests
 import sys
 import logging
+import tempfile
 from requests.auth import HTTPBasicAuth
 from retrying import retry, RetryError
+
+
+def get_report_file_name(target_directory, component_id):
+    return target_directory + "/" + component_id[component_id.rindex("/") + 1:len(component_id)].replace(":", "-") \
+           + "-" + datetime.now().strftime("%H:%M:%S").replace(":", "-") + ".json"
+
+
+def save_report(target_directory, report_as_json, component_id):
+    filename = get_report_file_name(target_directory, component_id)
+    with open(filename, "w") as output:
+        json.dump(report_as_json, output, indent=4, sort_keys=True)
+    logging.info(f"Successfully saved report to {filename}")
 
 
 class Artifactory:
@@ -35,12 +50,13 @@ class Artifactory:
         return f"{self.base_url}/xray/api/v1"
 
 
-class ScanOperation:
+class ArtifactScan:
 
     def __init__(self, artifactory: Artifactory, component_id: str, repo_key: str):
         self._artifactory = artifactory
         self._component_id = component_id
         self._component_path = self.convert_component_id_to_path()
+        self._report_name = self._component_path.replace("/", "-").replace(".", "-")
         self._repo_key = repo_key
         self._report_id = None
 
@@ -92,12 +108,14 @@ class ScanOperation:
         return self._component_id[type_index:last_colon_idx] + "/" + self._component_id[last_colon_idx+1:]
 
     def get_report(self):
-        return self.create_report()
+        self.create_report()
+        self.wait_for_report_creation()
+        return self.get_report_details()
 
     def create_report(self):
         logging.info(f"Start report creation for artifact {self._component_id}")
-        json = {
-            "name": f"{self._repo_key}",
+        request_data = {
+            "name": f"{self._report_name}",
             "resources": {
                 "repositories": [
                     {
@@ -108,19 +126,25 @@ class ScanOperation:
             "filters": {
                 "impacted_artifact": f"{self._component_id}",
                 "severities": [
-                    "High",
                     "Critical"
                 ]
             }
         }
         response = self._artifactory.session.post(f"{self._artifactory.xray_api_url}/reports/vulnerabilities",
-                                                  json=json)
+                                                  json=request_data)
         if response.status_code != 200:
             logging.info(f"Report creation for artifact {self._component_id} could not be started")
             return None
 
         self._report_id = response.json()["report_id"]
-        self.wait_for_report_creation()
+
+    def get_report_details(self):
+        response = self._artifactory.session.post(f"{self._artifactory.xray_api_url}/reports/vulnerabilities/{self._report_id}?direction=desc&page_num=1&num_of_rows=100&order_by=severity")
+        if response.status_code != 200:
+            logging.info(f"Report details for artifact {self._component_id} could not be retrieved")
+            return None
+        else:
+            return response.json()
 
     def retry_if_not_yet_completed(result):
         if not result:
@@ -151,19 +175,41 @@ class ScanOperation:
         return True
 
     def delete_report(self):
-        pass
+        response = self._artifactory.session.delete(f"{self._artifactory.xray_api_url}/reports/{self._report_id}")
+        if response.status_code != 200:
+            logging.error(f"Report {self._report_id} could not be deleted: {response.text}")
+            return False
+        else:
+            logging.debug(f"Report {self._report_id} successfully deleted")
 
 
-if __name__ == '__main__':
+class ArtifactReportAnalysis:
+
+    def __init__(self, component_id, vulnerability_report):
+        self._component_id = component_id
+        self._vulnerability_report = vulnerability_report
+
+    def contains_critical_vulnerabilities(self):
+        for row in self._vulnerability_report["rows"]:
+            if row["severity"] == "Critical":
+                logging.critical(f"Critical vulnerability found: {row}")
+                return True
+        logging.info(f"No critical vulnerability found")
+        return False
+
+
+def init_logging():
     root = logging.getLogger()
     root.setLevel(logging.DEBUG)
-
     handler = logging.StreamHandler(sys.stdout)
     handler.setLevel(logging.DEBUG)
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     handler.setFormatter(formatter)
     root.addHandler(handler)
 
+
+def parse_args():
+    global args
     parser = argparse.ArgumentParser()
     parser.add_argument("--user", help="The artifactory user name")
     parser.add_argument("--token", help="The api token of the user")
@@ -171,14 +217,15 @@ if __name__ == '__main__':
                         help="The component id. E.g.: 'docker://myrepo/path/component:5.0.50'")
     parser.add_argument("--repo-key", help="The repo-key")
     parser.add_argument("--base-url", help="The url to artifactory")
-    args = parser.parse_args()
+    parser.add_argument("--report-target-directory", help="The target directory to save the report to",
+                        default=tempfile.gettempdir())
+    return parser.parse_args()
 
-    artifactory = Artifactory(base_url=args.base_url, user=args.user, token=args.token)
-    scan_operation = ScanOperation(artifactory, args.component_id, args.repo_key)
 
-    if True:
+def start_and_wait_for_scan():
+    if True: # not artifact_scan.is_scanned():
         logging.info(f"Artifact {args.component_id} has not yet been scanned. Starting scan")
-        if not scan_operation.scan():
+        if not artifact_scan.scan():
             logging.error(f"Artifact {args.component_id} could not be scanned. Aborting")
             exit(1)
     else:
@@ -186,11 +233,46 @@ if __name__ == '__main__':
                      f"Aborting to avoid redundant report creation")
         exit(1)
 
-    report = scan_operation.get_report()
+
+def get_and_store_report():
+    global report
+    try:
+        report = artifact_scan.get_report()
+        save_report(args.report_target_directory, report, args.component_id)
+    except Exception as e:
+        logging.error("An error occurred while trying to get the vulnerability report", e)
+    finally:
+        artifact_scan.delete_report()
     if not report:
         logging.error(f"Vulnerability report for artifact {args.component_id} could not created. Aborting")
         exit(1)
+    logging.info(f"Vulnerability report for artifact {args.component_id} successfully obtained")
+    return report
 
-    # TODO: Store report here
-    scan_operation.delete_report()
+
+def analyse_report():
+    logging.info(f"Report for artifact {args.component_id} successfully obtained. Starting analysis")
+    analysis = ArtifactReportAnalysis(args.component_id, report)
+    if analysis.contains_critical_vulnerabilities():
+        logging.critical(f"Report for artifact {args.component_id} contains critical vulnerabilities")
+        exit(1)
+    else:
+        logging.info(f"Report for artifact {args.component_id} does not contains critical vulnerabilities")
+        exit(0)
+
+
+if __name__ == '__main__':
+    init_logging()
+
+    args = parse_args()
+
+    artifactory = Artifactory(base_url=args.base_url, user=args.user, token=args.token)
+    artifact_scan = ArtifactScan(artifactory, args.component_id, args.repo_key)
+
+    start_and_wait_for_scan()
+
+    report = get_and_store_report()
+
+    analyse_report()
+
 
--- xray-scanning/test_scan.py
+++ xray-scanning/test_scan.py
@@ -1,10 +1,10 @@
 import unittest
-from scan import ScanOperation
+from scan import ArtifactScan
 
 
 class ScanOperationTest(unittest.TestCase):
 
     def test_convert_component_id_to_path(self):
-        scan_operation = ScanOperation(None, "docker://myrepo/path/component:5.0.50", None)
+        scan_operation = ArtifactScan(None, "docker://myrepo/path/component:5.0.50", None)
         component_path = scan_operation.convert_component_id_to_path()
         self.assertEqual(component_path, "myrepo/path/component/5.0.50")
